8f23f81dc868915ed218c689ef3ee507c3d5b3bc
CaturAriya <70553420+CaturAriya@users.noreply.github.com>
2021-02-22 15:50:25 +0700

    poke

---
+++ lib/Ability.dart
@@ -0,0 +1,135 @@
+import 'package:flutter/material.dart';
+import 'package:pokemon/Detail.dart';
+
+class Ability extends StatelessWidget {
+  final Detail pokedetail;
+  const Ability({Key key, this.pokedetail}) : super(key: key);
+
+  bodyWidget() => Container();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          elevation: 0.0,
+          backgroundColor: Colors.red,
+          title: Text(
+            pokedetail.name,
+            style: TextStyle(fontSize: 20),
+          ),
+        ),
+        body: SingleChildScrollView(
+            child: Container(
+                child: Card(
+                  child: Column(
+                    children: [
+                      Image(
+                        image: NetworkImage(pokedetail.sprites, scale: 0.5),
+                      ),
+                      Row(
+                        children: [
+                          Container(
+                            child: Column(
+                              children: [],
+                            ),
+                          ),
+                          Text("Pokemon Name : ", style: TextStyle(fontSize: 20)),
+                          Text(
+                            pokedetail.name,
+                            style: TextStyle(fontSize: 20),
+                          ),
+                        ],
+                      ),
+                      Text(
+                        "",
+                        style: TextStyle(fontSize: 20),
+                      ),
+                      Row(
+                        children: [
+                          Text("Pokemon Height : ", style: TextStyle(fontSize: 20)),
+                          Text(
+                            '${pokedetail.height / 10} m',
+                            style: TextStyle(fontSize: 20),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Text("Pokemon Weight : ", style: TextStyle(fontSize: 20)),
+                          Text(
+                            '${pokedetail.weight / 10} kg',
+                            style: TextStyle(fontSize: 20),
+                          ),
+                        ],
+                      ),
+                      Text(
+                        "",
+                        style: TextStyle(fontSize: 20),
+                      ),
+                      Container(
+                          child: Text(
+                            "Types : ",
+                            style: TextStyle(fontSize: 20),
+                          )),
+                      //types
+                      Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+                        Column(
+                          children: [
+                            Container(
+                              width: 80,
+                              height: 80,
+                              child: ListView.builder(
+                                itemCount: pokedetail.types.length,
+                                itemBuilder: (BuildContext context, int index) {
+                                  return Column(
+                                    children: [
+                                      Container(
+                                        child: Text(
+                                          "-${pokedetail.types[index]}",
+                                          style: TextStyle(fontSize: 20),
+                                        ),
+                                      )
+                                    ],
+                                  );
+                                },
+                              ),
+                            )
+                          ],
+                        )
+                      ]),
+                      Container(
+                          child: Text(
+                            "Moves : ",
+                            style: TextStyle(fontSize: 20),
+                          )),
+                      Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+                        Column(
+                          children: [
+                            Container(
+                              width: 200,
+                              height: 200,
+                              child: ListView.builder(
+                                itemCount: pokedetail.moves.length,
+                                itemBuilder: (BuildContext context, int index) {
+                                  return Column(
+                                    children: [
+                                      Container(
+                                        child: Text(
+                                          "-${pokedetail.moves[index]}",
+                                          style: TextStyle(fontSize: 20),
+                                        ),
+                                      )
+                                    ],
+                                  );
+                                },
+                              ),
+                            )
+                          ],
+                        )
+                      ])
+                    ],
+                  ),
+                )))
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,145 @@
+import 'package:flutter/material.dart';
+import 'package:pokemon/Ability.dart';
+import 'package:pokemon/Detail.dart';
+import 'package:pokemon/PokeLink.dart';
+import 'Detail.dart';
+import 'package:flutter/cupertino.dart.';
+
+void main()=>runApp(MaterialApp(
+  title: "PokeApp",
+  home: HomePage(),
+  debugShowCheckedModeBanner: false,
+));
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+class _HomePageState extends State<HomePage> {
+  List<Detail> pokelist;
+  List<Detail> get pokemon => pokelist;
+  int num = 0;
+  bool isSearching = false;
+  getPokeData() async {
+    if (pokelist == null) {
+      // ignore: deprecated_member_use
+      pokelist = new List<Detail>();
+    }
+    if (num < 152) {
+      for (var n = 0; n < 5; n++) {
+        num++;
+        print('pulling data from pokemon index number ${num.toString()}');
+
+        var pull = await Pokepull.fetchpokemon(num.toString());
+        if (pull != null) {
+          setState(() {
+            pokelist.add(Detail.fromJson(pull));
+          });
+        }
+      }
+      print(pokelist.length);
+      return pokelist;
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getPokeData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: !isSearching
+              ? Text("Pokeapp")
+              : TextField(
+              decoration: InputDecoration(
+                  icon: Icon(
+                    Icons.search,
+                    color: Colors.white,
+                  ),
+                  hintText: "search your pokemon here",
+                  hintStyle: TextStyle(color: Colors.grey))),
+          actions: <Widget>[
+            isSearching
+                ? IconButton(
+                icon: Icon(Icons.cancel),
+                onPressed: () {
+                  setState(() {
+                    this.isSearching = !this.isSearching;
+                  });
+                })
+                : IconButton(
+              icon: Icon(Icons.search),
+              onPressed: () {
+                setState(() {
+                  this.isSearching = !this.isSearching;
+                });
+              },
+            )
+          ],
+          backgroundColor: Colors.red,
+        ),
+        body: Container(
+            child: pokelist.length > 0
+                ? GridView.builder(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 10,
+                  mainAxisSpacing: 5.0,
+                  childAspectRatio: 1.5),
+              itemCount: pokelist.length,
+              itemBuilder: (
+                  BuildContext context,
+                  int index,
+                  ) {
+                return Card(
+                  elevation: 10.0,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0)),
+                  margin: EdgeInsets.fromLTRB(15.0, 12.0, 15.0, 12.0),
+                  child: Padding(padding: const EdgeInsets.all(6.0),
+                    child: Column(children: [
+                      Align(
+                        alignment: Alignment.topLeft,
+                        child: Text("#${pokelist[index].num} ${pokelist[index].name}",
+                        style: TextStyle(
+                            fontWeight: FontWeight.w700,
+                            fontSize: 11),
+                      )
+                   ),
+                    InkWell(
+                        onTap: () {
+                       Navigator.push(
+                          context,
+                            MaterialPageRoute(
+                             builder: (context) => Ability(
+                                 pokedetail: pokelist[index],
+                )));
+                SizedBox(height: 6.0);
+                },
+                        child: Container(
+                          width: 100,
+                          height: 80,
+                          decoration: BoxDecoration(
+                            image: DecorationImage(
+                                image: NetworkImage(
+                                    pokelist[index].sprites)),
+
+                          ),
+                        ))
+                  ]),
+                ),
+                );
+              },
+            )
+                : Container(
+                child: Center(
+                  child: CircularProgressIndicator(
+                    valueColor: new AlwaysStoppedAnimation<Color>(Colors.red),
+                  ),
+                ))));
+  }
+}
